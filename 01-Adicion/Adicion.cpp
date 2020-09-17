16d62c7e7139a81cdfbcc636738a5b4d72b3476b
jrabuffetti <35005722+jrabuffetti@users.noreply.github.com>
2020-09-17 08:51:00 -0300

    Add files via upload

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int num1, num2;
+	std::cout << "Ingrese el primer numero:\n";
+	std::cin >> num1;
+	std::cout << "Ingrese el segundo numero:\n";
+	std::cin >> num2;
+	std::cout << "La suma de los dos numeros es: " << num1 + num2;
+
+	return 0;
+}
